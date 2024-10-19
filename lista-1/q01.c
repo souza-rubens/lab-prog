d2d3ec230f3b7b6f126dfcaed52aedcbcbf2d0bd
Rubens <99306045+souza-rubens@users.noreply.github.com>
2024-10-19 09:27:34 -0300

    Update q01.c

--- lista-1/q01.c
+++ lista-1/q01.c
@@ -3,8 +3,8 @@ int main(){
     int nDecimal;
     printf("Digite um numero decimal:\n");
     scanf("%d", &nDecimal);
-    printf("Numero em octagonal: %o", nDecimal);
-    printf("\nNumero em hexadecimal: %x", nDecimal);
+    printf("Numero em octagonal: %#o\n", nDecimal);
+    printf("Numero em hexadecimal: %#x\n", nDecimal);
 
     return 0;
 }
