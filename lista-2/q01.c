93a0e40ffd7bc630e69d863d4bc95abb3d4fed25
Rubens <99306045+souza-rubens@users.noreply.github.com>
2024-11-04 08:36:06 -0300

    Add files via upload

---
+++ lista-2/q01.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(){
+    unsigned char numDia;
+    puts("---- DIA DA SEMANA ----");
+    while(1){
+        puts("Digite um número de 1 a 7 para o dia ou 0 para encerrar o programa:");
+        scanf("%hhu", &numDia);
+        
+        if(!numDia){
+            puts("Programa encerrado pelo usuário");
+            return 0;
+        }
+        printf("Dia da semana %hhu: ", numDia);
+        switch(numDia){
+            case 1:
+                puts("Domingo");
+                break;
+            case 2:
+                puts("Segunda");
+                break;
+            case 3:
+                puts("Terça");
+                break;
+            case 4:
+                puts("Quarta");
+                break;
+            case 5:
+                puts("Quinta");
+                break;
+            case 6:
+                puts("Sexta");
+                break;
+            case 7:
+                puts("Sábado");
+                break;
+            default:
+                puts("Número de dia não válido");
+        }
+        puts("-----------------------");
+
+    }
+
+
+}
